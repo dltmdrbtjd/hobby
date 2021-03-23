ec414c1e96dde87519c995bc5e0bdbf23576984d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-03-24 00:07:35 +0900

    Goods관련페이지 작업완료
    모달창은 아직 제작안함

---
+++ src/js/Goods.js
@@ -0,0 +1,5 @@
+const GoodsItem = document.getElementById("goods-item1-1");
+
+GoodsItem.addEventListener("click", function(){
+    location.href = "Goods_buy.html";
+});
---
+++ src/js/Goods_buy.js
@@ -0,0 +1,5 @@
+const PaymentBtn = document.getElementById("Payment");
+
+PaymentBtn.addEventListener("click", function() {
+    location.href = "Goods_buy_Success.html"
+})
