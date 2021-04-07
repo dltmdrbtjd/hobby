099ca427fef5a56dec9be878c4542da1f2cba467
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-04-07 17:24:09 +0900

    Program 관련 페이지 완성
    우선 디자인 시안 나온 부분까지는
    Program페이지 포함 모든 페이지 완성

--- src/js/ProgramInfo.js
+++ src/js/ProgramInfo.js
@@ -1,14 +1,11 @@
+// 상세내용 펼쳐보기 toggle 제작중 ( 닫기버튼 아직 구현못함 )
+// const programInfo = document.getElementById("Program_info");
+// const programInfoContents = document.getElementById("Program_info_contents");
+// const programInfoBtn = document.getElementById("Program_info_btn");
 
-const programInfo = document.getElementById("Program_info");
-const programInfoContents = document.getElementById("Program_info_contents");
-const programInfoBtn = document.getElementById("Program_info_btn");
-
-programInfo.addEventListener("click", function() {
-    if(programInfoContents.style.display = "none") {
-        programInfoContents.style.display = "block";
-        programInfoBtn.innerText = "상세내용 닫기 ▲";
-    } else {
-        programInfoContents.style.display = "none";
-        programInfoBtn.innerText = "상세내용 펼펴보기 ▼";
-    }
-});
+// programInfo.addEventListener("click", function() {
+//     if(programInfoContents.style.display = "none") {
+//         programInfoContents.style.display = "block";
+//         programInfoBtn.innerText = "상세내용 닫기 ▲";
+//     }
+// });
